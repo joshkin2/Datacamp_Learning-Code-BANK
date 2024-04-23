8448a325bf883e0bde01910c152c08f383c414f9
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-04-23 17:11:29 +0100

    Update codes.py

--- codes.py
+++ codes.py
@@ -4,3 +4,14 @@ plt.hist(x,y), plt.scatter(x,y), plt.plot(x,y)
 plt.show()
 # build histogram with 5 bins. few bins= oversimplicity, too many bins= overcomplicate and hinder bigger picture
 plt.hist(x or y, bins=5)
+# clean up plot
+plt.clf()
+# labelling graphs, adding title, starting y from zero
+plt.xlabel("year")
+plt.ylabel("population")
+plt.title("World Population Projections")
+plt.yticks([0,2,4,6,8,10])
+plt.show()
+# add data to graph source
+year = [1800 ,1850 ,1900] + year
+pop = [1.0, 1.262, 1.650] + pop
