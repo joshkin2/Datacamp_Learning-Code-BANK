bb9a95b51bedea477901bcbc9247d91dc9523f31
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-04-23 16:25:57 +0100

    Create codes.py
    My code bank

---
+++ codes.py
@@ -0,0 +1,6 @@
+# plot histogram, scatterplot, graph
+plt.hist(x,y), plt.scatter(x,y), plt.plot(x,y)
+# display graph- nothing goes in parenthesis
+plt.show()
+# build histogram with 5 bins. few bins= oversimplicity, too many bins= overcomplicate and hinder bigger picture
+plt.hist(x or y, bins=5)
