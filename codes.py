a470f8f91a8f2086d39715f98be8800fde29ceeb
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-04-27 09:11:21 +0100

    Update codes.py

--- codes.py
+++ codes.py
@@ -32,13 +32,25 @@ world = {"afghan":30.55, "albania":2.77. "algreria":39.21} # dict
 # to find value of albania in dict use
 world["albania"]
 # get the index of albania in list
-ind_albania = countries.index("albania"
+ind_albania = countries.index("albania")
 # to print out keys
 print(europe.keys())		
-# add more data to dict
-world["sealand"] = 0.23                              
+# add more data to dict or update an info in dict
+world["sealand"] = int, float or str                           
 # check if a value is in a dict
 "sealand" in world
 # delete value from dict
 del(world["sealand"])
-#i need to do better with my coding time
+#chain square brackets to select elements
+europe['spain']['population']
+# Correct way to add a key-value pair to a dictionary
+data = {'capital': 'rome', 'population': 59.83}
+europe['italy'] = data
+print(europe)
+#turn dict into dataframe called cars
+cars = pd.DataFrame(my_dict)
+#specify row labels in a table
+
+
+
+
