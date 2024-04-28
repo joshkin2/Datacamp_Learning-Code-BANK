5c3ba1c7b9fe0368820ea2859e4d86d3000c9d1c
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-04-28 01:34:17 +0100

    Update codes.py

--- codes.py
+++ codes.py
@@ -59,6 +59,10 @@ brics[1:4] to extract info from the 2nd row to the 4th(n-1)
 brics.loc['RU'] or brics.loc[['RU']]
 #for multiple rows
 brics.loc[['RU', 'IN', 'CH']]
+# extract bool value of all data in a colunm as Series
+dr = brics['column number']
+#to do the above with one line of code
+sel = brics[brics['column no']]
 #extracting fro, beginning to end
 brics.loc[:, ['column1', 'column2']]
 #extract sub-DF
@@ -95,4 +99,27 @@ else :
 brics["area"] > 8 #print bool of data in the column area that is larger than 8
 # save the above as is_huge, then index it in the main DF to create a subset DF which will contain only values from DF larger than 8
 brics[is_huge]
-    
+# to get the above done in one line of code
+brics[brics['area'] > 8]
+# to compare values in a DF
+import numpy as np
+np.logical_and(brics['area'] > 8, brics['area'] < 10 #to get the bool values
+# or
+brics[np.logical_and(brics['area'] > 8, brics['area'] < 10)]
+# using while loop
+error = 50.0
+while error > 1:
+    error = error/4
+    print(error)  #this wil make error keep getting divided by 4 till it gets to "1"
+
+
+
+
+
+
+
+
+
+
+
+
