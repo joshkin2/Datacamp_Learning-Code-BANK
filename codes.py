ce63c6031be9466ff8c590dd8481689587a04116
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-06-22 20:11:17 +0100

    Update codes.py

--- codes.py
+++ codes.py
@@ -912,9 +912,32 @@ dogs.pivot_table(values='weight_kg', index='color')
 #Multiple statistics with pivot table
 dogs.pivot_table(values='weight_kg', index='color', aggfunc=[np.mean, np.median])
 #Pivot on two variables
-dogs.pivot_table(values='weight_kg', index='color', columns'breed')
+dogs.pivot_table(values='weight_kg', index='color', columns='breed')
 #filling missing values in pivot tables
 dogs.pivot_table(values='weight_kg', index='color', columns='breed', fill_value=0)
+#summing to get mean of column and row in pivot tables
+dogs.pivot_table(values='weight_kg', index='color', columns='breed', fill_value=0, margins=True)
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_table(values="weekly_sales", index="type")
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values="weekly_sales", index="type", aggfunc=[np.mean, np.median])
+# Pivot for mean weekly_sales by store type and holiday 
+mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales", index="type", columns="is_holiday")
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
