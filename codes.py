c94ef13701ccdeb05d69e69f4c3c9d54254d69f9
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-09-10 22:42:02 +0100

    Update codes.py

--- codes.py
+++ codes.py
@@ -1,5 +1,13 @@
 # plot histogram, scatterplot, graph
 plt.hist(x,y), plt.scatter(x,y), plt.plot(x,y)
+# Print variance and sd of co2_emission for each food_category
+print(food_consumption.groupby('food_category')['co2_emission'].agg([np.var, np.std]))
+# Create histogram of co2_emission for food_category 'beef'
+food_consumption[food_consumption['food_category'] == 'beef']['co2_emission'].hist()
+plt.show()
+# Create histogram of co2_emission for food_category 'eggs'
+food_consumption[food_consumption['food_category'] == 'eggs']['co2_emission'].hist()
+plt.show()
 # display graph- nothing goes in parenthesis
 plt.show()
 # build histogram with 5 bins. few bins= oversimplicity, too many bins= overcomplicate and hinder bigger picture
