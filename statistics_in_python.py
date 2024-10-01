dc1b1fe24c46f534e4c82d00d4b0ac4cebd89c9a
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-10-01 22:18:16 +0100

    Update statistics_in_python.py

--- statistics_in_python.py
+++ statistics_in_python.py
@@ -150,8 +150,10 @@ norm.ppf(0.9,161,7)
 norm.ppf((1-0.9), 161, 7)
 # generate random heights/ numbers
 norm.rvs(161,7, size=10)
-
-
+# Histogram of amount with 10 bins and show plot
+amir_deals['amount'].hist( bins= 10)
+plt.show()
+# Central limit theorem
 
 
 
