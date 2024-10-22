18f8b074778f1ede291b4476506c3be3f122cda4
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-10-22 17:02:44 +0100

    Update Supervised-learning-with-scikit-learn.py

--- Supervised-learning-with-scikit-learn.py
+++ Supervised-learning-with-scikit-learn.py
@@ -5,3 +5,40 @@
 from sklearn.module import Model
 model = Model()
 mode.fit(X, y)
+# KNN model using scikit-learn
+from sklearn.neighbors import KNeighborsClassifier
+X = churn_df[['total_day_charge', 'total_eve_charge']].values
+Y = churn_df[['churn']].values
+knn = KNeighborsClassifier(n_neighbors=15)
+knn.fit(X, Y)                 #  fitting classifier on labeled data
+X_new = np.array([[9,9],      # predicitng on unlabeled data
+                  [9,9],
+                  [9,9]])
+predictions = knn.predict(X_new)
+print('Predicitions: {}'.format(predictions))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
