955d45788de5b98e91a1e3be9155558f9753d06a
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-10-22 15:29:26 +0100

    Update Supervised-learning-with-scikit-learn.py

--- Supervised-learning-with-scikit-learn.py
+++ Supervised-learning-with-scikit-learn.py
@@ -1,3 +1,7 @@
-feature = predictor var. = independent var.
-target var. = dependent var. = response var
+#feature = predictor var. = independent var.
+#target var. = dependent var. = response var
 # K-NEAREST NEIGHBORS MODEL USES DISTANCE BTW OBSERVATIONS TO PREDICT LABLES OR VALUES
+# SUpervised learning workflow
+from sklearn.module import Model
+model = Model()
+mode.fit(X, y)
