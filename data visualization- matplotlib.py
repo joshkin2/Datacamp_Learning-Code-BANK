c5df6bc4a401167ed6d96f155a42bd387ac3ec5e
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-11-13 17:22:28 +0100

    Update data visualization- matplotlib.py

--- data visualization- matplotlib.py
+++ data visualization- matplotlib.py
@@ -2,14 +2,20 @@
 import matplotlib.pyplot as plt
 fig, ax= plt.subplots()
 # adding data to axes
-ax.plot(seattle_we['MONTH'], seattle_we['MLY-TAVG-NORMA'])   # plot 1
-ax.plot(austin_we['MONTH'], austin_we['MLY-TAVG-NORMA'])   # plot 2
+ax.plot(seattle_we['MONTH'], seattle_we['MLY-TAVG-NORMA']) #Plot MLY-PRCP-NORMAL from seattle_weather against the MONTH
+ax.plot(austin_we['MONTH'], austin_we['MLY-TAVG-NORMA']) # Plot MLY-PRCP-NORMAL from austin_weather against MONTH
 plt.show()
-
-
-
-
-
+#CUSTOMIZING DATA APPEARANCE
+# CIRCLE MARKERS - o, v
+ax.plot(seattle_we['MONTH'], seattle_we['MLY-TAVG-NORMA'], marker='o')
+# SETTING LINESTYLE- '--', 'None'
+ax.plot(seattle_we['MONTH'], seattle_we['MLY-TAVG-NORMA'], marker='v', linestyle='--')
+# CHOOSING COLOR
+ax.plot(seattle_we['MONTH'], seattle_we['MLY-TAVG-NORMA'], marker='v', linestyle='--', color='r')
+# CUSTOMIZE AXES LABELS
+ax.set_xlabel('Time (months)')
+# ADDING TITLE
+ax.set_title('Weather in Seattle')
 
 
 
