3c18de6268059471e7d3df41aae43bfc28dae2d8
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-11-18 16:52:31 +0100

    Update data visualization- matplotlib.py

--- data visualization- matplotlib.py
+++ data visualization- matplotlib.py
@@ -59,14 +59,36 @@ plt.show()
 # Zooming in on a decade
 sixties= climate_change['1960-01-01':'1968-12-31']
 fig, ax=plt.subplots()
-ax.plot(sixties.index, climate_change['co2'])
+ax.plot(sixties.index, sixties['co2'])
 ax.set_xlabel('Time')
 ax.set_ylabel('Co2 (ppm)')
 plt.show()
 #Read CSV,parse the "date" column as dates,set the "date" column as the index
 climate_change= pd.read_csv('climate_change.csv', parse_dates=['date'],index_col='date')
-
-
+# PLOTTING TIME SEREIS IN SAME SUBPLOT - twin axes
+fig, ax= plt.subplots()
+ax.plot(climate_change.index, climate_change['co2'], color='blue')
+ax.tick_params('y', colors='blue')  # to color y tick
+ax.set_xlabel('Time')
+ax.set_ylabel('CO2 (ppm)', color= 'blue')
+ax2= ax.twinx()
+ax2.plot(climate_change.index, climate_change['relative_temp'], color='red')
+ax2.set_ylabel('Relative temperature (Celsius)', color='red')
+ax2.tick_params('y', colors='red')  # to color y tick
+plt.show()
+# function that plots time-series
+def plot_timeseries(axes,x,y, color, xlabel,ylabel):
+  axes.plot(x,y,color=color)
+  axes.set_xlabel(xlabel)
+  axes.set_ylabel(ylabel, color=color)
+  axes.tick_params('y', colors=color)
+# using function above
+fig, ax= plt.subplots()
+plot_timeseries(ax,climate_change.index, climate_change['co2']),'b', 'Time','CO2 (ppm)')
+ax2= ax.twinx()
+plot_timeseries(ax2, climate_change.index, climate_change['relative_temp'],'r', 'Time',
+                'Relative temperature (Celsius)')
+plt.show()
 
 
 
