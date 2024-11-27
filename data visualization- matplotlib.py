189ffc057adfaf378ea04c439e998e156d899fef
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-11-27 17:02:14 +0100

    Update data visualization- matplotlib.py

--- data visualization- matplotlib.py
+++ data visualization- matplotlib.py
@@ -184,7 +184,34 @@ ax.set_ylabel('Height (cm)')
 plt.show()
 # SCATTER PLOTS - BIVARIATE COMPARISON
 fig, ax= plt.subplots()
-ax.scatter
+ax.scatter(climate_change["co2"], climate_change["relative_tmp"])
+ax.set_xlabel("CO2 (ppm)")
+ax.set_ylabel("Relative temperature (Celsius)")
+plt.show()
+# CUSTOMIZING SCATTER PLOTS
+eighties= climate_change['1980-01-01':'1989-12-31']
+nineties= climate_change['1990-01-01':'1999-12-31']
+fig, ax= plt.subplots()
+ax.scatter(eighties['co2'], eighties['relative_temp'],
+           color='red',label='eighties')
+ax.scatter(nineties['co2'], nineties['relative_temp'],
+           color='blue',label='nineties')
+ax.legend()
+ax.set_xlabel('CO2 (ppm)')
+ax.set_ylabel('Relative temperature (Celsius)')
+plt.show()
+# ENCODIING A 3RD VARIABLE BY COLOR
+fig,ax= plt.subplots()
+ax.scatter(climate_change['co2'], climate_change['relative_temp'],
+           c=climate_change.index)
+ax.set_xlabel('CO2 (ppm)')
+ax.set_ylael('Relative temperature (Celsius)')
+plt.show()
+# CHOOSIING STYLE OF PLOT
+plt.style.use("ggplot")
+plt.style.use("bmh") # add this line of code to line plot
+plt.style.use("default") # to change back to default- visit matplotlib.org for more styles
+plt.style.use("seaborn-colorblind"        
 
 
 
@@ -207,11 +234,4 @@ ax.scatter
 
 
 
-
-
-
-
-
-
-
-
+        
