4b1bd1476788e391e93784b5fdd62566b2c1b85c
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-01-03 17:15:42 +0100

    Update data visualization- seaborn.py

--- data visualization- seaborn.py
+++ data visualization- seaborn.py
@@ -45,6 +45,98 @@ plt.show()
 # wrapping columns, add= col_wrap=2
 #ordering columns, add= col_order=['Thur','Fri','Sat','Sun']
 
+#CUSTOMIZING SCATTER PLOTS
+#subgroups with point size and hue
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='total_bill',y='tip',data=tips,kind='scatter',size='size',hue='size')
+plt.show()
+#subgroups with point style
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='total_bill',y='tip',data=tips,kind='scatter',style='smoker',hue='smoker')
+plt.show()
+# changing point transparency
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='total_bill',y='tip',data=tips,kind='scatter',alpha=0.4)
+plt.show()
+# LINE PLOTS
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='hour',y='NO_2_mean',data=air_df_mean,kind='line')
+plt.show()
+# subgroups by location
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='hour',y='NO_2_mean',data=air_df_loc_mean,kind='line',style='location',hue='location')
+plt.show()
+# Adding markers
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='hour',y='NO_2_mean',data=air_df_loc_mean,kind='line',style='location',hue='location',markers=True)
+plt.show()
+# Turning off line style
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='hour',y='NO_2_mean',data=air_df_loc_mean,kind='line',style='location',hue='location',markers=True,dashes=False)
+plt.show()
+# Multiple observations per x-value - shaded region is confidence interval
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='hour',y='NO_2',data=air_df,kind='line')
+plt.show()
+# replace confidence interval with standard deviation- show spread of distribution
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.relplot(x='hour',y='NO_2',data=air_df,kind='line',ci='sd') # set ci to none to turn off
+plt.show()
+
+# COUNT PLOT
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.catplot(x='how_masculine',data=masculinity,kind='count')
+plt.show()
+# changing order
+import seaborn as sns
+import matplotlib.pyplot as plt
+category_order= ['No answer','Not at all','Not very','Somewhat','Very']
+sns.catplot(x='how_masculine',data=masculinity,kind='count',order=category_order)
+plt.show()
+# BAR PLOT
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.catplot(x='day',y='total_bill',data=tips,kind='bar')
+plt.show()
+# off confidence intervals
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.catplot(x='day',y='total_bill',data=tips,kind='bar',ci=None)
+plt.show()
+# change orientation of bars
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.catplot(x='total_bill',y='day',data=tips,kind='bar')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
