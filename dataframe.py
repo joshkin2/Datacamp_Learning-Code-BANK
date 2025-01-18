ecf98848b7ad815869c5659b4dd8e020fcbbb939
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-01-18 11:22:59 +0100

    Create dataframe.py

---
+++ dataframe.py
@@ -0,0 +1,56 @@
+#specify row labels in a table
+brics.index = ["infos to add"]
+#extracting infos as pandas DF in whole columns in a DF
+brics[['column1', 'column2']]
+#use slicing to extract rows in DF
+brics[1:4] to extract info from the 2nd row to the 4th(n-1)
+#pinpoint and extract all data in a row as panda series and panda DF on a DF using loc
+brics.loc['RU'] or brics.loc[['RU']]
+#for multiple rows
+brics.loc[['RU', 'IN', 'CH']]
+# extract bool value of all data in a colunm as Series
+dr = brics['column number']
+#to do the above with one line of code
+sel = brics[brics['column no']]
+
+#extracting from beginning to end
+brics.loc[:, ['column1', 'column2']]
+
+#extract sub-DF
+cars.loc['IN', 'cars_per_cap']
+cars.iloc[3, 0]
+#or
+cars.loc[['IN', 'RU'], 'cars_per_cap']
+cars.iloc[[3, 4], 0]
+#or
+cars.loc[['IN', 'RU'], ['cars_per_cap', 'country']]
+cars.iloc[[3, 4], [0, 1]]
+
+# filtering pandas DF
+brics["area"] > 8 #print bool of data in the column area that is larger than 8
+# save the above as is_huge, then index it in the main DF to create a subset DF which will contain only values from DF larger than 8
+brics[is_huge]
+# to get the above done in one line of code
+brics[brics['area'] > 8]
+
+# to compare values in a DF
+import numpy as np
+np.logical_and(brics['area'] > 8, brics['area'] < 10 #to get the bool values
+# or
+brics[np.logical_and(brics['area'] > 8, brics['area'] < 10)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
