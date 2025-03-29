aacf85fd08db013ed451e0f915cfe0986695d11b
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-03-29 12:27:44 +0100

    Update Python Toolbox - Iterators, list comprehensions and generators.py

--- Python Toolbox - Iterators, list comprehensions and generators.py
+++ Python Toolbox - Iterators, list comprehensions and generators.py
@@ -1,4 +1,16 @@
-# ITERATING over iterables: NEXT()
+# ITERATING over iterables: NEXT() - prints each letter one by one
 word= 'Da'
 it = iter(word)
 next(it)
+# ITEATING AT ONCE WITH *
+word = 'Data'
+it = iter(word)
+print(*it)
+# ITERATING DICT
+pythonistas= {'hugo':'bowne', 'francis':'castro'}
+for key, vlaue in pythonistas.items():
+  print(key, value)
+# ITERATING OVER FILE CONNECTIONS
+file = open('file.txt')
+it= iter(file)
+print(next(it))
