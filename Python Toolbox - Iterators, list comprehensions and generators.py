a14089d3e295c710bfaea144f9408be59efa800a
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-04-02 08:56:02 +0100

    Update Python Toolbox - Iterators, list comprehensions and generators.py

--- Python Toolbox - Iterators, list comprehensions and generators.py
+++ Python Toolbox - Iterators, list comprehensions and generators.py
@@ -138,10 +138,25 @@ for chunk in pd.read_csv('data.csv',chunksize=1000):
   result.append(sum(chunk['x']))
 total = sum(result)
 print(total)   # this gives sum of a column x
-
-
-
-
+# OR
+import pandas as pd
+total= 0
+for chunk in pd.read_csv('data.csv',chunksize=1000):
+ total+=sum(chunk['x'])
+print(total)
+# EXERCISE 1
+# Initialize an empty dictionary: counts_dict
+counts_dict= {}
+# Iterate over the file chunk by chunk
+for chunk in pd.read_csv('tweets.csv',chunksize=10):
+    # Iterate over the column in DataFrame
+    for entry in chunk['lang']:
+        if entry in counts_dict.keys():
+            counts_dict[entry] += 1
+        else:
+            counts_dict[entry] = 1
+# Print the populated dictionary
+print(counts_dict)
 
 
 
