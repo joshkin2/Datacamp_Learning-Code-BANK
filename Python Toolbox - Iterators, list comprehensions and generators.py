7b944ee4256b74c8acb0b0e3508e9a10746be8ae
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-04-09 10:45:02 +0100

    Update Python Toolbox - Iterators, list comprehensions and generators.py

--- Python Toolbox - Iterators, list comprehensions and generators.py
+++ Python Toolbox - Iterators, list comprehensions and generators.py
@@ -319,6 +319,109 @@ tweet_clock_time = [entry[11:19] for entry in tweet_time if entry[17:19] == '19'
 # Print the extracted times
 print(tweet_clock_time)
 
+# CASE STUDY OF ALL LEARNT IN THIS SECTION
+# Zip lists: zipped_lists
+zipped_lists = zip(feature_names,row_vals)
+# Create a dictionary: rs_dict
+rs_dict = dict(zipped_lists)
+# Print the dictionary
+print(rs_dict)
+# function to do the above across many rows of data
+# Define lists2dict()
+def lists2dict(list1, list2):
+    """Return a dictionary where list1 provides
+    the keys and list2 provides the values."""
+    # Zip lists: zipped_lists
+    zipped_lists = zip(list1, list2)
+    # Create a dictionary: rs_dict
+    rs_dict = dict(zipped_lists)
+    # Return the dictionary
+    return rs_dict
+# Call lists2dict: rs_fxn
+rs_fxn = lists2dict(feature_names,row_vals)
+# Print rs_fxn
+print(rs_fxn)
+#USE FUNC ABOVE WITH LIST COMP TO CONVERT BUNCH OF LISTS TO LIST OF DICTS
+# Print the first two lists in row_lists
+print(row_lists[0])
+print(row_lists[1])
+# Turn list of lists into list of dicts: list_of_dicts
+# list comprehension that generates a dictionary using lists2dict() for each sublist in row_lists. 
+#The keys are from the feature_names list and the values are the row entries in row_lists. Use sublist as your iterator variable
+list_of_dicts = [lists2dict(feature_names,sublist) for sublist in row_lists]
+# Print the first two dictionaries in list_of_dicts
+print(list_of_dicts[0])
+print(list_of_dicts[1])
+# Turning this all into a DataFrame
+# Import the pandas package
+import pandas as pd
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
+# Turn list of dicts into a DataFrame: df
+df = pd.DataFrame(list_of_dicts)
+# Print the head of the DataFrame
+print(df.head())
+
+# CASE STUDY 2 - Processing data in chunks (1)
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+    # Skip the column names
+    file.readline()
+    # Initialize an empty dictionary: counts_dict
+    counts_dict = {}
+    # Process only the first 1000 rows
+    for j in range(1000):
+        # Split the current line into a list: line
+        line = file.readline().split(',')
+        # Get the value for the first column: first_col
+        first_col = line[0]
+        # If the column value is in the dict, increment its value
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+        # Else, add to the dict and set value to 1
+        else:
+            counts_dict[first_col] = 1
+# Print the resulting dictionary
+print(counts_dict)
+
+# Writing a generator to load data in chunks (2)
+# Define read_large_file()
+def read_large_file(file_object):
+    """A generator function to read a large file lazily."""
+    # Loop indefinitely until the end of the file
+    while True:
+        # Read a line from the file: data
+        data = file_object.readline()
+        # Break if this is the end of the file
+        if not data:
+            break
+        # Yield the line of data
+        yield data        
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+    # Create a generator object for the file: gen_file
+    gen_file = read_large_file(file)
+    # Print the first three lines of the file
+    print(next(gen_file))
+    print(next(gen_file))
+    print(next(gen_file))
+
+# Writing a generator to load data in chunks (3)
+# Initialize an empty dictionary: counts_dict
+counts_dict = {}
+
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+    # Iterate over the generator from read_large_file()
+    for line in read_large_file(file):
+        row = line.split(',')
+        first_col = row[0]
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+        else:
+            counts_dict[first_col] = 1
+# Print            
+print(counts_dict)
 
 
 
