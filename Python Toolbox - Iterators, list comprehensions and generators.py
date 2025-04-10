84ce592fc8799a5bfdd6e20696bee89a83afd554
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-04-10 09:18:09 +0100

    Update Python Toolbox - Iterators, list comprehensions and generators.py

--- Python Toolbox - Iterators, list comprehensions and generators.py
+++ Python Toolbox - Iterators, list comprehensions and generators.py
@@ -423,6 +423,111 @@ with open('world_dev_ind.csv') as file:
 # Print            
 print(counts_dict)
 
+# EXERCISE
+#Writing an iterator to load data in chunks (1)
+# Import the pandas package
+import pandas as pd
+# Initialize reader object: df_reader
+df_reader = pd.read_csv('ind_pop.csv', chunksize=10)
+# Print two chunks
+print(next(df_reader))
+print(next(df_reader))
+#Writing an iterator to load data in chunks (2)
+# Initialize reader object: urb_pop_reader
+urb_pop_reader = pd.read_csv('ind_pop_data.csv', chunksize=1000)
+# Get the first DataFrame chunk: df_urb_pop
+df_urb_pop = next(urb_pop_reader)
+# Check out the head of the DataFrame
+print(df_urb_pop.head())
+# Check out specific country: df_pop_ceb
+df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode']=='CEB']
+# Zip DataFrame columns of interest: pops
+pops = zip(df_pop_ceb['Total Population'], df_pop_ceb['Urban population (% of total)'])
+# Turn zip object into list: pops_list
+pops_list = list(pops)
+# Print pops_list
+print(pops_list)
+#Writing an iterator to load data in chunks (3)
+# Code from previous exercise
+urb_pop_reader = pd.read_csv('ind_pop_data.csv', chunksize=1000)
+df_urb_pop = next(urb_pop_reader)
+df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
+pops = zip(df_pop_ceb['Total Population'], 
+           df_pop_ceb['Urban population (% of total)'])
+pops_list = list(pops)
+# Use list comprehension to create new DataFrame column 'Total Urban Population'
+df_pop_ceb['Total Urban Population'] = [int(pop[0]*pop[1]/100) for pop in pops_list]
+# Plot urban population data
+df_pop_ceb.plot(kind='scatter', x='Year', y='Total Urban Population')
+plt.show()
+#Writing an iterator to load data in chunks (4)
+# Initialize reader object: urb_pop_reader
+urb_pop_reader = pd.read_csv('ind_pop_data.csv', chunksize=1000)
+# Initialize empty DataFrame: data
+data = pd.DataFrame()
+# Iterate over each DataFrame chunk
+for df_urb_pop in urb_pop_reader:
+    # Check out specific country: df_pop_ceb
+    df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
+    # Zip DataFrame columns of interest: pops
+    pops = zip(df_pop_ceb['Total Population'],
+                df_pop_ceb['Urban population (% of total)'])
+    # Turn zip object into list: pops_list
+    pops_list = list(pops)
+    # Use list comprehension to create new DataFrame column 'Total Urban Population'
+    df_pop_ceb['Total Urban Population'] = [int(tup[0] * tup[1] * 0.01) for tup in pops_list]    
+    # Concatenate DataFrame chunk to the end of data: data
+    data = pd.concat([data,df_pop_ceb])
+# Plot urban population data
+data.plot(kind='scatter', x='Year', y='Total Urban Population')
+plt.show()
+#Writing an iterator to load data in chunks (5)
+# Define plot_pop()
+def plot_pop(filename, country_code):
+    # Initialize reader object: urb_pop_reader
+    urb_pop_reader = pd.read_csv(filename, chunksize=1000)
+    # Initialize empty DataFrame: data
+    data = pd.DataFrame()    
+    # Iterate over each DataFrame chunk
+    for df_urb_pop in urb_pop_reader:
+        # Check out specific country: df_pop_ceb
+        df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == country_code]
+        # Zip DataFrame columns of interest: pops
+        pops = zip(df_pop_ceb['Total Population'],
+                    df_pop_ceb['Urban population (% of total)'])
+        # Turn zip object into list: pops_list
+        pops_list = list(pops)
+        # Use list comprehension to create new DataFrame column 'Total Urban Population'
+        df_pop_ceb['Total Urban Population'] = [int(tup[0] * tup[1] * 0.01) for tup in pops_list]        
+        # Concatenate DataFrame chunk to the end of data: data
+        data = pd.concat([data, df_pop_ceb])
+    # Plot urban population data
+    data.plot(kind='scatter', x='Year', y='Total Urban Population')
+    plt.show()
+# Set the filename: fn
+fn = 'ind_pop_data.csv'
+# Call plot_pop for country code 'CEB'
+plot_pop(fn,'CEB')
+# Call plot_pop for country code 'ARB'
+plot_pop(fn,'ARB')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
