1fd6e25b39755411a8abd144973669fd824f48dc
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2025-04-28 08:59:19 +0100

    Update Exploratory_DA.py

--- Exploratory_DA.py
+++ Exploratory_DA.py
@@ -193,9 +193,28 @@ salaries['std_dev']= salaries.groupby('Experience')['Salary_usd'].transform(lamb
 print(salaries[['Experience','std_dev']].value_counts())
 salaries['median_by_comp_size']= salaries.groupby('Company_size')\['Salary_usd'].transform(lambda x:x.median())
 
+# EXERCISE
+# Preview the column
+print(planes["Duration"].head())
+# Remove the string character
+planes["Duration"] = planes["Duration"].str.replace("h", "")
+# Convert to float data type
+planes["Duration"] = planes["Duration"].astype(float)
+# Plot a histogram
+sns.histplot(data=planes,x="Duration")
+plt.show()
+# ADD DESCRIPTIVE STATS TO THE ABOVE EXERCISE
+# Price standard deviation by Airline
+planes["airline_price_st_dev"] = planes.groupby("Airline")["Price"].transform(lambda x: x.std())
+print(planes[["Airline", "airline_price_st_dev"]].value_counts())
+# Median Duration by Airline
+planes["airline_median_duration"] = planes.groupby("Airline")["Price"].transform(lambda x: x.median())
+print(planes[["Airline","airline_median_duration"]].value_counts())
+# Mean Price by Destination
+planes["price_destination_mean"] = planes.groupby("Destination")["Price"].transform(lambda x: x.mean())
+print(planes[["Destination","price_destination_mean"]].value_counts())
 
-
-
+# HANDLING OUTLIERS
 
 
 
